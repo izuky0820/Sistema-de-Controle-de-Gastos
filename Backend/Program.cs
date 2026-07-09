b838ad2d6c54124a88b153a3aa2c088c98257f43
Evelyn <1613698@sga.pucminas.br>
2026-07-09 12:36:01 -0300

    criação das telas de pessoa

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,8 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
 builder.Services.AddSingleton<JsonService>();
+
+// Configuração do CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("React",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,6 +26,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Permite que o frontend React faça requisições para esta API
+app.UseCors("React");
 
 app.MapControllers();
 
---
+++ frontend/src/pages/Pessoas.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import type { Pessoa } from "../types/Pessoa";
+import PessoaList from "../components/PessoaList";
+import PessoaForm from "../components/PessoaForm";
+
+/*
+ * Página responsável pelo gerenciamento das pessoas.
+ *
+ * Nesta etapa ela apenas:
+ * - Consulta a API;
+ * - Armazena as pessoas;
+ * - Envia a lista para o componente PessoaList.
+ */
+function Pessoas() {
+
+    /*
+     * Estado responsável por armazenar todas as pessoas
+     * retornadas pela API.
+     */
+    const [pessoas, setPessoas] = useState<Pessoa[]>([]);
+
+    /*
+     * Executa apenas quando a página é carregada.
+     */
+    useEffect(() => {
+
+        carregarPessoas();
+
+    }, []);
+
+    /*
+     * Busca todas as pessoas cadastradas.
+     */
+    async function carregarPessoas() {
+
+        try {
+
+            const resposta = await api.get("/pessoas");
+
+            setPessoas(resposta.data);
+
+        }
+        catch (erro) {
+
+            console.error("Erro ao carregar pessoas.", erro);
+
+        }
+
+        
+
+    }
+
+    /*
+ * Exclui uma pessoa cadastrada.
+ *
+ * Após a exclusão, a lista é carregada novamente
+ * para manter a interface sincronizada com o backend.
+ */
+        async function excluirPessoa(id: number) {
+
+            const confirmar = window.confirm(
+                "Deseja realmente excluir esta pessoa?"
+            );
+
+            if (!confirmar)
+                return;
+
+            try {
+
+                await api.delete(`/pessoas/${id}`);
+
+                await carregarPessoas();
+
+            }
+            catch (erro) {
+
+                console.error("Erro ao excluir pessoa.", erro);
+
+                alert("Não foi possível excluir.");
+
+            }
+
+        }
+
+    return (
+
+        <div>
+
+            <PessoaForm atualizarLista={carregarPessoas} />
+            <PessoaList
+                pessoas={pessoas}
+                excluirPessoa={excluirPessoa}
+            />
+
+            <hr />
+
+
+            
+
+        </div>
+
+    );
+
+}
+
+export default Pessoas;
